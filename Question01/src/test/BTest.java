5fc5b01c689a8519ec5d0271069684f5e3e193bb
afdafczl <96963890+afdafczl@users.noreply.github.com>
2022-10-23 23:54:56 +0800

    Add files via upload

---
+++ Question01/src/test/BTest.java
@@ -0,0 +1,12 @@
+package test;
+import com.oo.aa.*;
+import com.oo.bb.*;
+import com.oo.cc.*;
+
+public class BTest {
+	public static void main(String[] args) {
+		System.out.println("b test starts");
+		B b = new B(1);
+		System.out.println("yeah~");
+	}
+}
